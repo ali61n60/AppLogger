73f7a3228387b07a163ef37c0e0ca67cd11a0ab5
Ali <ali62n62@yahoo.com>
2017-07-23 22:05:18 +0430

    nuget package created

---
+++ AppLogger/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AppLogger
+{
+    //https://docs.microsoft.com/en-us/nuget/create-packages/publish-a-package
+    //https://docs.microsoft.com/en-us/nuget/guides/create-net-standard-packages-vs2017
+    public class Logger
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
